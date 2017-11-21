65d587d81881b094c288405a9c5e870cd1b641c9
Sebastian Ferraro <sferrar0@tesla.inf.rosario.gov.ar>
2017-11-21 13:35:59 -0300

    first commit

---
+++ LoRaSensor.ino
@@ -0,0 +1,45 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+// WIFI_LoRa_32 ports
+// GPIO5  -- SX1278's SCK
+// GPIO19 -- SX1278's MISO
+// GPIO27 -- SX1278's MOSI
+// GPIO18 -- SX1278's CS
+// GPIO14 -- SX1278's RESET
+// GPIO26 -- SX1278's IRQ(Interrupt Request)
+
+#define SS      5
+#define RST     14
+#define DI0     26
+
+int counter = 0;
+
+void setup() {
+  Serial.begin(115200);
+  SPI.begin(18, 19, 23, 5);
+  LoRa.setPins(SS, RST, DI0);
+  while (!Serial);
+
+  Serial.println("LoRa Sender");
+
+  if (!LoRa.begin(915E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+
+  // send packet
+  LoRa.beginPacket();
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  counter++;
+
+  delay(5000);
+}
