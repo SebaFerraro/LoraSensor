374f3cf6aae9667cc9acebfc318b174168f29699
sferraro <sferraro@isef11.com.ar>
2017-11-22 21:10:04 -0300

    Prueba envia y recibe

---
+++ LoRaSensor/LoraRceptor/LoraRceptor.ino
@@ -0,0 +1,51 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+// WIFI_LoRa_32 ports
+// GPIO5  -- SX1278's SCK
+// GPIO19 -- SX1278's MISO
+// GPIO27 -- SX1278's MOSI
+// GPIO18 -- SX1278's CS
+// GPIO14 -- SX1278's RESET
+// GPIO26 -- SX1278's IRQ(Interrupt Request)
+#define LED_BUILTIN 2
+#define SS      5
+#define RST     14
+#define DI0     26
+
+void setup() {
+  Serial.begin(115200);
+  SPI.begin(18, 19, 23, 5);
+  LoRa.setPins(SS, RST, DI0);
+  pinMode(LED_BUILTIN, OUTPUT);
+  while (!Serial);
+
+  Serial.println("LoRa Receiver");
+
+  if (!LoRa.begin(915E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
+    }
+
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);                       // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    delay(100);                      
+  }
+}
